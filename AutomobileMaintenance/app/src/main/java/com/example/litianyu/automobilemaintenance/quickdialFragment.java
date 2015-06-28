494ed4cbc996bae0fb33b6b7d12c07bb701a5afa
ltianyu <litianyu@litianyudeMacBook-Pro.local>
2015-06-28 16:05:17 -0400

    main activity layout

---
+++ AutomobileMaintenance/app/src/main/java/com/example/litianyu/automobilemaintenance/quickdialFragment.java
@@ -0,0 +1,23 @@
+package com.example.litianyu.automobilemaintenance;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class quickdialFragment extends Fragment {
+
+    public quickdialFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_quickdial, container, false);
+    }
+}
---
+++ AutomobileMaintenance/app/src/main/java/com/example/litianyu/automobilemaintenance/remainderFragment.java
@@ -0,0 +1,23 @@
+package com.example.litianyu.automobilemaintenance;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class remainderFragment extends Fragment {
+
+    public remainderFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_remainder, container, false);
+    }
+}
