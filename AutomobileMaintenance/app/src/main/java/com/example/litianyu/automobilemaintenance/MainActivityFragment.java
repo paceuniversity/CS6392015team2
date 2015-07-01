53747fc4450fbe0a338c9cc2ba78f3f0d8c140dd
Brenden <furp7691@yahoo.com>
2015-06-30 20:00:42 -0400

    Added Reminders and click events

--- AutomobileMaintenance/app/src/main/java/com/example/litianyu/automobilemaintenance/MainActivityFragment.java
+++ AutomobileMaintenance/app/src/main/java/com/example/litianyu/automobilemaintenance/MainActivityFragment.java
@@ -1,16 +1,20 @@
 package com.example.litianyu.automobilemaintenance;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements View.OnClickListener {
 
     public MainActivityFragment() {
     }
@@ -18,6 +22,47 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button SETUP = (Button)rootView.findViewById(R.id.button);
+        SETUP.setOnClickListener(this);
+
+        Button MAINT = (Button)rootView.findViewById(R.id.button2);
+        MAINT.setOnClickListener(this);
+
+        Button REMIND = (Button)rootView.findViewById(R.id.button3);
+        REMIND.setOnClickListener(this);
+
+        Button DIAL = (Button)rootView.findViewById(R.id.button4);
+        DIAL.setOnClickListener(this);
+
+        return rootView;
     }
+
+    @Override
+    public void onClick(View v) {
+
+
+        switch(v.getId()){
+            case R.id.button:
+                final Intent activitysetup = new Intent(getActivity(),setup.class);
+                startActivity(activitysetup);
+                break;
+            case R.id.button2:
+                final Intent activitymaint = new Intent(getActivity(),MainActivity.class);
+                startActivity(activitymaint);
+                break;
+            case R.id.button3:
+                final Intent activityremind = new Intent(getActivity(),remainder.class);
+                startActivity(activityremind);
+                break;
+            case R.id.button4:
+                final Intent activityintent = new Intent(getActivity(),quickdial.class);
+                startActivity(activityintent);
+                break;
+        }
+    }
+
+
+
 }
